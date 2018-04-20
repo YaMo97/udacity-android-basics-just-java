299f143bf75abadf0d360c4773ae12f1540f1b07
Yatharth Manocha <yathman97@gmail.com>
2018-04-20 21:53:58 +0530

    Part 8 - Add TextViews for Price

--- app/src/main/java/com/udacity/android/basics/justjava/MainActivity.java
+++ app/src/main/java/com/udacity/android/basics/justjava/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -20,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2 * 50);
     }
 
     /**
@@ -31,5 +34,15 @@ public class MainActivity extends AppCompatActivity {
                 R.id.quantity_text_view
         );
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view
+        );
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
