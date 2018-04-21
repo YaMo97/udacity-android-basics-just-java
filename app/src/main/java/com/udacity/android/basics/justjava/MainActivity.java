63f44bd7ea2a6e9c7c02a12078bb44a7db8106d4
Yatharth Manocha <yathman97@gmail.com>
2018-04-21 16:43:29 +0530

    Part 20 - Modify the decrement() method

--- app/src/main/java/com/udacity/android/basics/justjava/MainActivity.java
+++ app/src/main/java/com/udacity/android/basics/justjava/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         int quantity = 2;
-        quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
