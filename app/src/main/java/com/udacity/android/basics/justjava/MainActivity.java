248236acbb7ce6eec65f409d15034f71a77c6d63
Yatharth Manocha <yathman97@gmail.com>
2018-04-21 22:30:16 +0530

    Part 8: Combining Strings Together - Display item count and total price on submitting order

--- app/src/main/java/com/udacity/android/basics/justjava/MainActivity.java
+++ app/src/main/java/com/udacity/android/basics/justjava/MainActivity.java
@@ -40,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
-        displayMessage(priceMessage);
+        String message = "Item Count:\t" + quantity + " Coffees\n"
+                + "Total Price:\t" + NumberFormat.getCurrencyInstance().format(quantity * 50);
+        displayMessage(message);
     }
 
     /**
